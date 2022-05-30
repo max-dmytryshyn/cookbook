fd70979d7518110d09e959f50b3aefca391c6a13
max-dmytryshyn <maksumus22@gmail.com>
2022-05-30 16:30:45 +0300

    Configure prettier + eslint

---
+++ Frontend/cookbook/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest'
+  },
+  plugins: ['react', '@typescript-eslint', 'import', 'react-hooks'],
+  rules: {}
+};
