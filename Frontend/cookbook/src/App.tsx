f1e58a96be854ba5ba2519348401a93040d24e11
max-dmytryshyn <maksumus22@gmail.com>
2022-05-30 17:59:45 +0300

    Rewrite header and footed usage

--- Frontend/cookbook/src/App.tsx
+++ Frontend/cookbook/src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import styles from './App.module.scss';
 import { Route, Routes } from 'react-router-dom';
 import { CategoriesPage } from './components/category/CategoriesPage';
+import { Header } from './components/header/Header';
+import { Footer } from './components/footer/Footer';
 
 function App() {
   return (
     <div className={styles.App}>
+      <Header />
       <Routes>
-        <Route path="/" element={<CategoriesPage />} />
+        <Route path="/" element={<div></div>} />
         <Route path="/categories" element={<CategoriesPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- Frontend/cookbook/src/components/category/CategoriesPage.tsx
+++ Frontend/cookbook/src/components/category/CategoriesPage.tsx
@@ -4,37 +4,26 @@ import { Category } from '../../types/CookbookTypes';
 import { CategoryItem } from './CategoryItem';
 import { GET_ALL_CATEGORIES_QUERY, getAllCategoriesQueryDataType } from './gql/Queries';
 import styles from './CategoriesPage.module.scss';
-import { Header } from '../header/Header';
-import { Footer } from '../footer/Footer';
 
 export const CategoriesPage: React.FC = () => {
   const { loading, error, data } = useQuery<getAllCategoriesQueryDataType | undefined>(
     GET_ALL_CATEGORIES_QUERY
   );
 
-  let body;
-  if (loading) body = <p>Loading...</p>;
-  else if (error) body = <p>Error</p>;
-  else
-    body = (
-      <div className={styles.categoriesContainer}>
-        {data &&
-          data.allCategories.map((category: Category) => (
-            <CategoryItem
-              id={category.id as number}
-              name={category.name as string}
-              ingredients={category.ingredients as []}
-              key={category.id}
-            />
-          ))}
-        <button className={styles.addCategoryButton}>+</button>
-      </div>
-    );
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
   return (
-    <div>
-      <Header />
-      {body}
-      <Footer />
+    <div className={styles.categoriesContainer}>
+      {data &&
+        data.allCategories.map((category: Category) => (
+          <CategoryItem
+            id={category.id as number}
+            name={category.name as string}
+            ingredients={category.ingredients as []}
+            key={category.id}
+          />
+        ))}
+      <button className={styles.addCategoryButton}>+</button>
     </div>
   );
 };
